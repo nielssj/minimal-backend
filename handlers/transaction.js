6a5d4be2b0dfa0cf1ffe61b1919c898a1b8f9f47
Niels Søholm <nm@9la.dk>
2017-10-25 13:30:56 +0200

    Move lambda files into handlers

--- handlers/transaction.js
+++ handlers/transaction.js
@@ -1,14 +1,31 @@
 const transactionService = require('../services/transactionService');
+const dbUtils = require('../db/dbUtils');
 
 module.exports = {
-  post: (req, res, next) => {
-    const inTransaction = req.body;
-    return transactionService.createTransaction(inTransaction, req.user)
-      .then(outTransaction => res.json(outTransaction))
-      .catch(next);
+  post: (transaction, context, callback) => {
+    // const user = { id: context.authorizer.principalId };
+    const user = { id: 'bc79214b-3361-4f30-8411-d31b66e15873' }; // FIXME: When auth implemented get from context (see previous line)
+    transactionService.createTransaction(transaction, user)
+      .then((result) => {
+        dbUtils.close();
+        callback(null, result);
+      })
+      .catch((err) => {
+        dbUtils.close();
+        callback(err);
+      });
   },
-  get: (req, res, next) =>
-    transactionService.readAllTransactions(req.user)
-    .then(transactions => res.json(transactions))
-    .catch(next),
+  get: (input, context, callback) => {
+    // const user = { id: context.authorizer.principalId };
+    const user = { id: 'bc79214b-3361-4f30-8411-d31b66e15873' }; // FIXME: When auth implemented get from context (see previous line)
+    transactionService.readAllTransactions(user)
+      .then((result) => {
+        dbUtils.close();
+        callback(null, result);
+      })
+      .catch((err) => {
+        dbUtils.close();
+        callback(err);
+      });
+  }
 };
